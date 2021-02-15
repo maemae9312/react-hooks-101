f54a7906caa42021160575e4c91e13071019c1f0
maemae9312 <hc081110@gmail.com>
2021-02-15 09:34:23 +0900

    create reducer for events

---
+++ src/reducers/index.js
@@ -0,0 +1,15 @@
+export default function events (state = [], action) {
+  switch(action.type) {
+    case 'CREATE_EVENT':
+      const event = { title: action.title, body: action.body }
+      const length = state.length
+      let id = length === 0 ? 1 : { id = state[length -1].id + 1}
+      return [...state, { id, ...event }]
+
+    case 'DELETE_EVENT':
+    case 'DELETE_ALL_EVENTS':
+      return []
+    default:
+      return state
+  }
+}
