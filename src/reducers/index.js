496910df70f3726193c3ba93049eb18c94c1d218
maemae9312 <hc081110@gmail.com>
2021-02-16 09:42:28 +0900

    implement event creation

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ export default function events (state = [], action) {
     case 'CREATE_EVENT':
       const event = { title: action.title, body: action.body }
       const length = state.length
-      let id = length === 0 ? 1 : { id = state[length -1].id + 1}
+      const id = length === 0 ? 1 : state[length -1].id + 1
       return [...state, { id, ...event }]
 
     case 'DELETE_EVENT':
