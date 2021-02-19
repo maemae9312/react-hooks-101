5defa60930e3ad25f3b9946bc5cbc5c6e602e172
maemae9312 <hc081110@gmail.com>
2021-02-19 10:55:04 +0900

    create operation log in case of event deletion

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,14 +1,26 @@
 import React, { useContext } from 'react'
 
-import { DELETE_EVENT } from '../actions'
+import {
+  ADD_OPERATION_LOG,
+  DELETE_EVENT
+} from '../actions'
 import AppContext from '../contexts/AppContext'
+import { timeCurrentIso8601 } from '../utils'
 
 export default function Event ({ event }) {
   const { dispatch } = useContext(AppContext)
   const id = event.id
   const handleClickDeleteButton = () => {
     const result = window.confirm(`Can I really delete the event?(id=${id})`)
-    if (result) dispatch({ type: DELETE_EVENT, id})
+    if (result){
+      dispatch({ type: DELETE_EVENT, id})
+
+      dispatch({
+        type: ADD_OPERATION_LOG,
+        description: `Deleted the event.(id=${id})`,
+        operatedAt: timeCurrentIso8601()
+      })
+    }
   }
 
   return (
